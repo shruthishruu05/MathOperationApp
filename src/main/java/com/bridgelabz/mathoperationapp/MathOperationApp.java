b6d73f44674aab9837a4e59b7f6478ce1ebaba76
shruthishruu05 <shruthi.m@perfios.com>
2021-09-13 20:04:31 +0530

    REFACTORED : using function to print the math operation results

--- src/main/java/com/bridgelabz/mathoperationapp/MathOperationApp.java
+++ src/main/java/com/bridgelabz/mathoperationapp/MathOperationApp.java
@@ -6,10 +6,10 @@ public class MathOperationApp {
 		IMathFunction subtract = (a,b) -> a-b;
 		IMathFunction multiply = ( a, b) -> a*b;
 		IMathFunction divide = (a,b) -> a/b;
-		System.out.println("ADDITION:"+add.calculate(7, 3));
-		System.out.println("SUBTRACTION:"+subtract.calculate(7, 3));
-		System.out.println("MULITIPLICATION:"+multiply.calculate(7, 3));
-		System.out.println("DIVISION:"+divide.calculate(20,2));
+		IMathFunction.print(7, 3,"ADD",add);
+		IMathFunction.print(7, 3,"SUBTRACT",subtract);
+		IMathFunction.print(7, 3,"MULTIPLY",multiply);
+		IMathFunction.print(7, 3,"DIVIDE",divide);
 
 	}
 
@@ -17,4 +17,9 @@ public class MathOperationApp {
 @FunctionalInterface
 interface IMathFunction{
 	int calculate(int a, int b);
+	static void print(int a, int b, String function, IMathFunction functionObj)
+	{
+		System.out.println("RESULT OF THE OPERATION "+function+" IS "+functionObj.calculate(a, b));
+	}
+
 }
